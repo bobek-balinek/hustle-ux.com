abbb05fe53adc18f2101968e2e3fcd8644224921
Przemyslaw Bobak <bobak.art@gmail.com>
2013-11-10 14:58:27 +0000

    refactor SCSS files

--- app/scripts/components/motion.js
+++ app/scripts/components/motion.js
@@ -97,6 +97,7 @@
 			}
 
 			options.lastFrame = frame;
+			return ;
 		};
 
 		var attachEvents = function(data){
@@ -110,6 +111,8 @@
 					options.isEnabled = true;
 				});
 			}
+
+			return;
 		};
 
 		var unbindEvents = function(key){
@@ -161,163 +164,3 @@
 	app.register('motion', motionComponent);
 
 })(jQuery, Leap, window);
-
-var width = $(window).width();
-var height = $(window).height();
-
-window.tt = null;
-
-window.clickedElement = false;
-
-/**
- * Example of 1 finger hover
- */
-var fingerTipElement = document.querySelector('.tip');
-
-var fingerCallback = function(frame){
-
-	// console.log(frame.fingers.length);
-
-	// for(var i=0; i < frame.fingers.length; i++){
-	if(frame.fingers[0]){
-		// $(fingerTipElement).removeClass('scroll');
-		fingerTipElement.className = 'tip';
-
-		var pos = frame.fingers[0].stabilizedTipPosition;
-		var poss = app.get('motion').leapToScene(frame, pos, width, height);
-
-		var deltaX = poss[0] + 44;
-		var deltaY = poss[1] + 44;
-
-		window.mouseX = deltaX;
-		window.mouseY = deltaY;
-
-		// $('.tip:eq('+i+')').css('left', deltaX);
-		// $('.tip:eq('+i+')').css('top', deltaY);
-
-		fingerTipElement.style.left = deltaX + 'px';
-		fingerTipElement.style.top = deltaY + 'px';
-
-		var gammaX = Math.abs(frame.fingers[0].tipVelocity[0]);
-		var gammaY = Math.abs(frame.fingers[0].tipVelocity[1]);
-		var gammaZ = Math.abs(frame.fingers[0].tipVelocity[2]);
-
-
-		// console.log(pos[2]);
-		// if( pos[2] < -50 ){
-		// 	var element = document.elementFromPoint( ( poss[0] + 44), (poss[1] + 44));
-		// 	$('.tip:eq('+i+')').addClass('in');
-
-		// 	if( !app.get('motion').timeouts('clickEvent') ){
-		// 		app.get('motion').addTimeout('clickEvent', setTimeout(function(){
-		// 			$(element).click('click');
-		// 			// $(element).trigger('click');
-		// 			console.log('click!', element);
-		// 			app.get('motion').removeTimeout('clickEvent');
-		// 			$('.tip:eq('+i+')').removeClass('in');
-		// 		},1000) );
-		// 	}
-		// }else{
-		// 	$('.tip:eq('+i+')').removeClass('in');
-		// }
-
-		if(gammaX < 2 && gammaY < 2 && gammaZ < 2){
-			// console.log(gammaX, gammaY, gammaZ);
-			// var tip = $('.tip:eq('+i+')')
-
-			$(fingerTipElement).addClass('animated pulse');
-
-			window.tt = setTimeout(function(){
-				$(fingerTipElement).removeClass('animated pulse');
-			},1000);
-		}
-	}
-};
-
-var backgroundCallback = function(frame){
-		var pos = frame.fingers[0].stabilizedTipPosition;
-		var poss = app.get('motion').leapToScene(frame, pos, width, height);
-
-		var velocX = Math.abs(frame.fingers[0].tipVelocity[0]);
-		var velocY = Math.abs(frame.fingers[0].tipVelocity[1]);
-
-		var deltaX = poss[0] + 44;
-		var deltaY = poss[1] + 44;
-
-		// console.log(frame.fingers[0]);
-
-		window.mouseX = deltaX * (velocX/ 100);
-		window.mouseY = deltaY * (velocY / 100);
-};
-
-/**
- * Example of Tap to select
- */
-var tapCallback = function(gesture, frame){
-	var cords = app.get('motion').leapToScene(frame, gesture.position, $(window).width(), $(window).height());
-
-	setTimeout(function(){
-		var element = document.elementFromPoint( ( cords[0] + 44), (cords[1] + 44));
-
-		// console.log(cords[0] + 44, cords[1] + 44);
-
-		var old = 1;
-
-		// console.log(element);
-
-		if( $(element).prop('tagName') !== 'HTML' || $(element).prop('tagName') !== 'BODY' ){
-
-			$(element).css('-webkit-transform','scale(0.9)');
-			setTimeout(function(){
-				$(element).css('-webkit-transform','scale(1)');
-			},400);
-		}
-
-	},200);
-};
-
-
-var swipeCallback = function(gesture, frame){
-
-	if(gesture.progress > 1 && gesture.radius < 25){
-		console.log('Listening....', gesture);
-		app.get('speech').start();
-	}
-
-};
-
-/**
- *Scrolling callback
- * @param  {[type]} frame   [description]
- * @param  {[type]} options [description]
- * @return {[type]}         [description]
- */
-var scrollCallback = function(frame, options){
-	var pos = frame.pointables[0].stabilizedTipPosition;
-	if(options.lastFrame.pointables.length > 0){
-		var delta = pos[1] - options.lastFrame.pointables[0].stabilizedTipPosition[1];
-
-		if( frame.hands[0] && frame.hands[0].palmVelocity ){
-			fingerTipElement.className = 'tip scroll';
-			// console.log(frame);
-			// $(window).scrollTop( $(window).scrollTop() + delta );
-			var veloc = Math.abs(frame.hands[0].palmVelocity[1]);
-			// console.log(veloc);
-			// window.scrollBy(0,delta);
-			var offset = ( $('.page-wrap').scrollTop() + (delta * (veloc / 100) ) );
-			// console.log(offset);
-			//
-			$('.page-wrap').scrollTop( offset );
-		}
-		//
-	}
-};
-
-if( !Modernizr.touch ){
-	app.get('motion').attachEvents({
-		'1fingers': fingerCallback,
-		'screenTap': tapCallback,
-		'3fingers': backgroundCallback,
-		'5fingers': scrollCallback
-	});
-}
---
+++ app/scripts/components/page.js
@@ -0,0 +1,126 @@
+
+var width = $(window).width();
+var height = $(window).height();
+
+window.tt = null;
+
+window.clickedElement = false;
+
+/**
+ * Example of 1 finger hover
+ */
+var fingerTipElement = document.querySelector('.tip');
+
+var fingerCallback = function(frame){
+
+	if(frame.fingers[0]){
+		fingerTipElement.className = 'tip';
+
+		var pos = frame.fingers[0].stabilizedTipPosition;
+		var poss = app.get('motion').leapToScene(frame, pos, width, height);
+
+		var deltaX = poss[0] + 44;
+		var deltaY = poss[1] + 44;
+
+		window.mouseX = deltaX;
+		window.mouseY = deltaY;
+
+		fingerTipElement.style.left = deltaX + 'px';
+		fingerTipElement.style.top = deltaY + 'px';
+
+		var gammaX = Math.abs(frame.fingers[0].tipVelocity[0]);
+		var gammaY = Math.abs(frame.fingers[0].tipVelocity[1]);
+		var gammaZ = Math.abs(frame.fingers[0].tipVelocity[2]);
+
+
+		if(gammaX < 2 && gammaY < 2 && gammaZ < 2){
+
+			$(fingerTipElement).addClass('animated pulse');
+
+			window.tt = setTimeout(function(){
+				$(fingerTipElement).removeClass('animated pulse');
+			},1000);
+		}
+	}
+};
+
+var backgroundCallback = function(frame){
+		var pos = frame.fingers[0].stabilizedTipPosition;
+		var poss = app.get('motion').leapToScene(frame, pos, width, height);
+
+		var velocX = Math.abs(frame.fingers[0].tipVelocity[0]);
+		var velocY = Math.abs(frame.fingers[0].tipVelocity[1]);
+
+		var deltaX = poss[0] + 44;
+		var deltaY = poss[1] + 44;
+
+		window.mouseX = deltaX * (velocX/ 100);
+		window.mouseY = deltaY * (velocY / 100);
+
+		return ;
+};
+
+/**
+ * Example of Tap to select
+ */
+var tapCallback = function(gesture, frame){
+	var cords = app.get('motion').leapToScene(frame, gesture.position, $(window).width(), $(window).height());
+
+	setTimeout(function(){
+		var element = document.elementFromPoint( ( cords[0] + 44), (cords[1] + 44));
+		var old = 1;
+
+		if( $(element).prop('tagName') !== 'HTML' || $(element).prop('tagName') !== 'BODY' ){
+
+			$(element).css('-webkit-transform','scale(0.9)');
+			setTimeout(function(){
+				$(element).css('-webkit-transform','scale(1)');
+			},400);
+		}
+
+	},200);
+
+	return ;
+};
+
+
+var swipeCallback = function(gesture, frame){
+
+	if(gesture.progress > 1 && gesture.radius < 25){
+		console.log('Listening....', gesture);
+		app.get('speech').start();
+	}
+
+};
+
+/**
+ * Scrolling callback
+ * @param  {[type]} frame   [description]
+ * @param  {[type]} options [description]
+ * @return {[type]}         [description]
+ */
+var scrollCallback = function(frame, options){
+	var pos = frame.pointables[0].stabilizedTipPosition;
+	if(options.lastFrame.pointables.length > 0){
+		var delta = pos[1] - options.lastFrame.pointables[0].stabilizedTipPosition[1];
+
+		if( frame.hands[0] && frame.hands[0].palmVelocity ){
+			fingerTipElement.className = 'tip scroll';
+			var veloc = Math.abs(frame.hands[0].palmVelocity[1]);
+			var offset = ( $('.page-wrap').scrollTop() + (delta * (veloc / 100) ) );
+			$('.page-wrap').scrollTop( offset );
+		}
+
+	}
+
+	return ;
+};
+
+if( !Modernizr.touch ){
+	app.get('motion').attachEvents({
+		'1fingers': fingerCallback,
+		'screenTap': tapCallback,
+		'3fingers': backgroundCallback,
+		'5fingers': scrollCallback
+	});
+}
